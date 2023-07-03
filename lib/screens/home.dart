2ae1d5a50e7820b2515fcf70bdbb89a7317fa94d
salem2001 <salemwd89@gmail.com>
2023-07-03 05:04:48 -0700

    added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -47,6 +47,7 @@ class _HomeState extends State<Home> {
                     height: 150,
                   ),
 
+
                   Image.asset(
                     images[index],
                     fit: BoxFit.cover,
